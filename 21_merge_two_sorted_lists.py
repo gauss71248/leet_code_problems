cb56e3f7eed2ded280a5cec781b2316fc307d833
Sebastian Schlesinger <Sebastian.Schlesinger@zalando.de>
2022-05-29 23:31:42 +0200

    small correction

--- 21_merge_two_sorted_lists.py
+++ 21_merge_two_sorted_lists.py
@@ -1,13 +1,12 @@
 # Definition for singly-linked list.
+from typing import Optional, Tuple
 class ListNode:
      def __init__(self, val=0, next=None):
          self.val = val
          self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: 
-Optional[ListNode]) -> Optional[ListNode]:
-        def process_min(l1: Optional[ListNode], l2: Optional[ListNode]) -> 
-(Optional[int],Optional[ListNode], Optional[ListNode]):
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        def process_min(l1: Optional[ListNode], l2: Optional[ListNode]) -> Tuple[Optional[int],Optional[ListNode], Optional[ListNode]]:
             if l1 is not None:
                 head_l1 = l1.val
                 next_l1 = l1.next
@@ -30,9 +29,12 @@ Optional[ListNode]) -> Optional[ListNode]:
             else:
                 return (head_l2, l1, next_l2)
         
-        def process_one(l1: Optional[ListNode], l2:Optional[ListNode], 
-work_list: Optional[ListNode], head: Optional[ListNode]) -> 
-Optional[ListNode]:
+        def process_one(
+            l1: Optional[ListNode], 
+            l2:Optional[ListNode],
+            work_list: Optional[ListNode], 
+            head: Optional[ListNode]
+            ) -> Optional[ListNode]:
             (min, next_l1, next_l2) = process_min(l1, l2)
             if min is None:
                 return head
